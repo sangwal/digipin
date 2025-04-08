f9dca24c8b78240a2b242929083c9ea97d702a16
CEPT-VZG <m.10145805@gov.in>
2025-04-08 07:06:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 console.log(`DIGIPIN API running at http://localhost:${PORT}`);
